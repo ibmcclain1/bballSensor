1a75a6c8f84772b90ba1fed8e0a9d14f41924423
ibmcclain1 <49536640+ibmcclain1@users.noreply.github.com>
2022-11-29 16:33:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ import argparse
 from pythonosc import udp_client
 import serial, time
 import numpy as np
+import tkinter as tk
+
+from tkinter import *
+from tkinter import ttk
+
 #import logging
 #import threading
+
+#Troubleshooting GUI
+#root = Tk()
+#frm = ttk.Frame(root, padding=10)
+#frm.grid()
+#ttk.Label(frm, text= 'Last Message').grid(column=0,row=0)
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", default="192.168.1.45",
@@ -15,7 +28,61 @@ args = parser.parse_args()
 
 client = udp_client.SimpleUDPClient(args.ip, args.port)
 
-ser = serial.Serial("/dev/serial0", 115200,timeout=0) # mini UART serial device
+#ser = serial.Serial("/dev/serial0", 115200,timeout=0) # mini UART serial device
+status = "Uninitialized "
+message = 'na'
+
+class App(tk.Tk):
+	def __init__(self):
+		super().__init__()
+		self.title('Basketball Sensor')
+		self.geometry('680x430')
+		self.resizable(0, 0)
+
+		self.create_header_frame()
+		self.create_body_frame()
+		self.create_footer_frame()
+
+	def create_header_frame(self):
+		self.header = ttk.Frame(self)
+		#grid setup
+		self.header.columnconfigure(0, weight=2)
+		self.header.columnconfigure(1, weight=5)
+		#self.header.columnconfigure(2, weight=5)
+
+		#label
+		self.label = ttk.Label(self.header, text='Status', width = 25)
+		self.label.grid(column = 0, row=0)
+
+		#label
+		self.label2 = ttk.Label(self.header, text=status, width = 100)
+		self.label2.grid(column = 1, row=0)
+
+		#Attach
+		self.header.grid(column=0, row=0, padx=10, pady=10)
+
+	def create_body_frame(self):
+		self.body = ttk.Frame(self)
+		#Grid Setup
+		self.body.columnconfigure(0, weight=5)
+		self.body.columnconfigure(1, weight=5)
+
+		#Most recent label
+		self.label = ttk.Label(self.body, text='Most recent message', width = 34)
+		self.label.grid(column = 0, row = 1)
+
+		#Most recent 
+		self.label = ttk.Label(self.body, text= message, width = 100)
+		self.label.grid(column = 1, row = 1)
+
+		#Attach
+		self.body.grid(column=0, row=1, padx=10, pady=10)
+
+	def create_footer_frame(self):
+		self.footer = ttk.Frame(self)
+
+
+		self.footer.grid(column=0, row=2, padx=10, pady=10)
 
 def read_tfluna_data():
     while True:
@@ -33,7 +100,7 @@ def read_tfluna_data():
 
 #### Main Loop ####
 
-while True:
+def sensorLoop():
 	####  Activate sensor and measure #####
 	if ser.isOpen() == False:
 	    ser.open() # open serial port if not open
@@ -42,14 +109,17 @@ while True:
 	print('Distance: {0:2.2f} m, Strength: {1:2.0f} / 65535 (16-bit), Chip Temperature: {2:2.1f} C'.\
 	              format(distance,strength,temperature)) # print sample data
 	
-	#ser.close() # close serial port
+	ser.close() # close serial port
+	status = 'Sensor Running'
 
 	#####    OSC     #######
-	#client.connect( (ip,port) )
-	if(distance > 0.1 and distance < 0.4):
-		client.send_message("/net", 1)
-	else :
-		client.send_message("/net", 0)
-
-
 	
+	if(distance > 0.1 and distance < 0.4):
+		client.send_message("/goal", 1)
+	else :
+		client.send_message("/goal", 0)
+
+
+app = App()
+app.after(50, sensorLoop())
+app.mainloop()
