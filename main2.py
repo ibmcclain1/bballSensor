cd00a1622bd11dc28b01b2cf2ff078c411b03c94
ibmcclain1 <49536640+ibmcclain1@users.noreply.github.com>
2022-11-30 12:31:07 -0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -61,10 +61,10 @@ def read_data():
                 print("Temperature:" + str(temperature) + "\n")
                 ser.reset_input_buffer()
 
-              if(distance > 0.1 and distance < 0.4):
-                client.send_message("/goal", 1)
-              else :
-                client.send_message("/goal", 0)
+                if(distance > 0.1 and distance < 0.4):
+                    client.send_message("/goal", 1)
+                else :
+                    client.send_message("/goal", 0)
 
 
 if __name__ == "__main__":
